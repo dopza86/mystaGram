f71205758bad77477ef7673cd4256eafa0b4f289
dopza86 <dopza86@gmail.com>
2020-11-22 22:48:08 +0900

    인증,jwt토큰

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
-import { sendSecretMail } from "./utils";
+import passport from "passport";
+import "./passport";
+import { authenticateJwt } from "./passport";
 
-sendSecretMail("dopza86@gmail.com", "1234");
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request }),
+});
+
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`http://localhost:${PORT} 에서 서버구동`)
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,14 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-const sendMail = (email) => {
+const sendEMail = (email) => {
   const options = {
     auth: {
       api_key: process.env.SENDGRID_PASSWORD,
@@ -25,8 +22,11 @@ export const sendSecretMail = (adress, secret) => {
   const email = {
     from: "dopza86@gmail.com",
     to: adress,
-    subject: "🔒Login Secret for Prismagram🔒",
-    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`,
+    subject: "🔒마이스타그램 로그인 시크릿키🔒",
+    html: `로그인 시크릿키는 <strong>${secret}</strong> 입니다.<br/>
+    로그인시 복사 붙여넣기 해주세요`,
   };
-  return sendMail(email);
+  return sendEMail(email);
 };
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
