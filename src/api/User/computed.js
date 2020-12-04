2c88b03fd5a0b7f8b78c697665967dc99a3f4224
dopza86 <dopza86@gmail.com>
2020-12-05 03:54:12 +0900

    3.14-3.20

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -23,14 +23,4 @@ export default {
       }
     },
   },
-  Post: {
-    isLiked: (parent, _, { request }) => {
-      const { user } = request;
-      const { id } = parent;
-
-      return prisma.$exists.like({
-        AND: [{ user: { id: user.id } }, { post: { id } }],
-      });
-    },
-  },
 };
